365e3a69c7c1ce5a95f4596049beaec117e361e1
HSdover <dover0601@naver.com>
2021-07-16 19:40:32 +0900

    Initial commit

---
+++ src/com/algorithm/greedy/prac.java
@@ -0,0 +1,10 @@
+package com.algorithm.greedy;
+
+public class prac {
+    public static void main(String[] args) {
+        int n = 10;
+        int m = n/0;
+
+//        System.out.println(m);
+    }
+}
