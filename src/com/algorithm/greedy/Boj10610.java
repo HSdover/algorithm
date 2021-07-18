a0cfa360244ef11b7bf208a00fb7de167f488b4a
HSdover <dover0601@naver.com>
2021-07-18 12:18:51 +0900

    백준 1783번

--- src/com/algorithm/greedy/Boj10610.java
+++ src/com/algorithm/greedy/Boj10610.java
@@ -20,7 +20,6 @@ public class Boj10610 {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String N = br.readLine();
 
