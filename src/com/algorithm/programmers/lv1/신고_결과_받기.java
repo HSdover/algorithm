3dcb6184a9f7bd6419eac695b4dfa49af5d232b4
HSdover <dover0601@naver.com>
2022-01-19 06:11:42 +0900

    lv1

--- src/com/algorithm/programmers/lv1/신고_결과_받기.java
+++ src/com/algorithm/programmers/lv1/신고_결과_받기.java
@@ -5,21 +5,40 @@ import java.util.*;
 public class 신고_결과_받기 {
     public static void main(String[] args) {
 
+        신고_결과_받기.solution();
     }
 
-    public int[] solution(String[] id_list, String[] report, int k) {
-        int[] answer = {};
+    public static int[] solution() {
+        String[] id_list = new String[]{"muzi", "frodo", "apeach", "neo"};
+        String[] report = new String[]{"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
+        int k = 2;
 
-        String[] deleteDupl = new String[id_list.length];
-        for(int i = 0; i < id_list.length; i++ )
-            deleteDupl[i] = id_list[i].concat("/" + report[i]);
+        int[] answer = new int[id_list.length];
 
-        Set<String> set = new HashSet<String>(Arrays.asList(deleteDupl));
+        // 같은 신고자가 신고한 중복 신고 지우기
+        Set<String> set = new HashSet<String>(Arrays.asList(report));
 
         List<String> result = new ArrayList<String>(set);
 
+        int[] cnt = new int[id_list.length];
+        for(int i = 0; i < result.size(); i++ ) {
+            //피신고자 아이디만 구하기
+            String id = result.get(i).substring(result.get(i).lastIndexOf(" ") + 1);
 
+            //신고 회수가 들어갈 배열 위치 지정
+            int idx = Arrays.asList(id_list).indexOf(id);
+            cnt[idx]++;
+        }
+
+
+        for(int i = 0; i < cnt.length; i++) {
+            if(cnt[i] >= k)
+                answer[i] = cnt[i] / 2;
+            else
+                answer[i] = 0;
+        }
 
         return answer;
+        //신고자 구분 필요
     }
 }
