eea921555678439b2ab6ab4f699d0148936b62bf
user <dover0601@naver.com>
2022-01-19 15:44:03 +0900

    lv1 신고 결과 받기 완

--- src/com/algorithm/programmers/lv1/신고_결과_받기.java
+++ src/com/algorithm/programmers/lv1/신고_결과_받기.java
@@ -20,25 +20,43 @@ public class 신고_결과_받기 {
 
         List<String> result = new ArrayList<String>(set);
 
+        // 아이디별 신고당한 횟수
         int[] cnt = new int[id_list.length];
+
+        int[][] cId = new int[id_list.length][id_list.length];
+
         for(int i = 0; i < result.size(); i++ ) {
             //피신고자 아이디만 구하기
             String id = result.get(i).substring(result.get(i).lastIndexOf(" ") + 1);
 
-            //신고 회수가 들어갈 배열 위치 지정
+            //신고 횟수가 들어갈 배열 위치 지정
             int idx = Arrays.asList(id_list).indexOf(id);
             cnt[idx]++;
+            //신고한 사람 확인하는 배열
+            cId[idx][Arrays.asList(id_list).indexOf(result.get(i).substring(0, result.get(i).indexOf(" ")))]++;
         }
 
-
-        for(int i = 0; i < cnt.length; i++) {
-            if(cnt[i] >= k)
-                answer[i] = cnt[i] / 2;
-            else
-                answer[i] = 0;
+        for(int i = 0; i < id_list.length; i++) {
+            if(cnt[i] >= k) {
+                for(int j = 0; j < id_list.length; j++) {
+                    if(cId[i][j] > 0)
+                        answer[j]++;
+                }
+            }
         }
+
+//        for(int i = 0; i < id_list.length; i++) {
+//            //신고당한 횟수가 k회 이상일 경우
+//            if(cnt[i] >= k) {
+//                for(int j = 0; j < result.size(); j++) {
+//                    if(id_list[i].equals(result.get(j).substring(result.get(j).lastIndexOf(" ") + 1))) {
+//                        String idxId = result.get(j).substring(0, result.get(j).indexOf(" "));
+//                        answer[Arrays.asList(id_list).indexOf(idxId)]++;
+//                    }
+//                }
+//            }
+//        }
 
         return answer;
-        //신고자 구분 필요
     }
 }
