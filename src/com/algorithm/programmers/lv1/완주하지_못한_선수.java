3cae311828e6c88bbf44475bb63b0ccd7bbc5e59
user <dover0601@naver.com>
2022-01-25 15:05:22 +0900

    lv1 완주하지 못한 선수 ~ing

---
+++ src/com/algorithm/programmers/lv1/완주하지_못한_선수.java
@@ -0,0 +1,15 @@
+package com.algorithm.programmers.lv1;
+
+public class 완주하지_못한_선수 {
+    public static void main(String[] args) {
+
+    }
+
+    public String solution() {
+        String[] participant;
+        String[] completion;
+        String answer = "";
+
+        return answer;
+    }
+}
