1234b97008d778a613383085e25069f2cfb4ae38
HSdover <dover0601@naver.com>
2022-02-03 06:04:22 +0900

    lv1 다트게임 ~ing

--- src/com/algorithm/programmers/lv1/다트게임.java
+++ src/com/algorithm/programmers/lv1/다트게임.java
@@ -2,10 +2,11 @@ package com.algorithm.programmers.lv1;
 
 public class 다트게임 {
     public static void main(String[] args) {
-
+        solution();
     }
 
-    public int solution(String dartResult) {
+    public static int solution() {
+        String dartResult;
         int answer = 0;
         return answer;
     }
