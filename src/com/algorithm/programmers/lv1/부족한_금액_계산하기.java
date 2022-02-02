dce746406959e89030f0a4b193435cc0c077ea7f
HSdover <dover0601@naver.com>
2022-02-02 11:47:49 +0900

    lv1 부족한 금액 계산하기 완

--- src/com/algorithm/programmers/lv1/부족한_금액_계산하기.java
+++ src/com/algorithm/programmers/lv1/부족한_금액_계산하기.java
@@ -1,4 +1,25 @@
 package com.algorithm.programmers.lv1;
 
 public class 부족한_금액_계산하기 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static long solution() {
+        int price = 3;
+        int money = 20;
+        int count = 4;
+        long answer = -1;
+
+        long sum = 0;
+        for(int i = 1; i <= count; i++) {
+            sum += price * i;
+        }
+
+        if(sum - money < 0)
+            answer = 0;
+        else
+            answer = sum - money;
+        return answer;
+    }
 }
