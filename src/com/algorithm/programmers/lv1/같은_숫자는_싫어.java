5118c29e74a9cf47b49a866d13d25282c66a7f15
user <dover0601@naver.com>
2022-02-03 16:11:32 +0900

    lv1 같은 숫자는 싫어 완

---
+++ src/com/algorithm/programmers/lv1/같은_숫자는_싫어.java
@@ -0,0 +1,33 @@
+package com.algorithm.programmers.lv1;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class 같은_숫자는_싫어 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static int[] solution() {
+        int[] arr = {4,4,4,3,3};
+
+        int temp = arr[0];
+        List<Integer> distinct = new ArrayList<>();
+        distinct.add(arr[0]);
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] != temp) {
+                distinct.add(arr[i]);
+                temp = arr[i];
+            }
+        }
+
+        int[] answer = new int[distinct.size()];
+
+        for(int i = 0; i < distinct.size(); i++)
+            answer[i] = distinct.get(i);
+
+        return answer;
+    }
+}
