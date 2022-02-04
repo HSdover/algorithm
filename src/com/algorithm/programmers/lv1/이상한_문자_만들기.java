a6f9959926935de5f2b81bb3d2727ee9a7dfe9bb
user <dover0601@naver.com>
2022-02-04 18:02:17 +0900

    lv1 이상한 문자 만들기 ~ing

---
+++ src/com/algorithm/programmers/lv1/이상한_문자_만들기.java
@@ -0,0 +1,30 @@
+package com.algorithm.programmers.lv1;
+
+import java.util.Arrays;
+import java.util.Locale;
+
+public class 이상한_문자_만들기 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static String solution() {
+        String s = "trydppgpgaf helloasdfga worldasdgasg";
+        String answer = "";
+
+        String[] word = s.split(" ");
+
+        for(int i = 0; i < word.length; i++) {
+            for(int j = 0; j < word[i].length(); j++) {
+                if(j % 2 == 0)
+                    answer += word[i].substring(j, j+1).toUpperCase(Locale.ROOT);
+                else if(j % 2 == 1)
+                    answer += word[i].substring(j, j+1).toLowerCase(Locale.ROOT);
+            }
+            if(i != word.length-1)
+                answer += " ";
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
