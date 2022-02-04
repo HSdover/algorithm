3154ea37ed47772c3f83f78a00b872e87d23fdc8
user <dover0601@naver.com>
2022-02-04 17:43:13 +0900

    lv1 약수의 합 완

--- src/com/algorithm/programmers/lv1/약수의_합.java
+++ src/com/algorithm/programmers/lv1/약수의_합.java
@@ -1,4 +1,22 @@
 package com.algorithm.programmers.lv1;
 
 public class 약수의_합 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static int solution() {
+        int n = 5;
+        int answer = 0;
+
+        for(int i = 1; i <= Math.sqrt(n); i++) {
+            if(i == Math.sqrt(n))
+                answer += i;
+            else if(n % i == 0)
+                answer += i + (n / i);
+        }
+        System.out.println(answer);
+
+        return answer;
+    }
 }
