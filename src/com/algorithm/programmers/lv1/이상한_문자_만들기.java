24d9ce69b5262894139bae5360838495511abaa1
HSdover <dover0601@naver.com>
2022-02-06 23:22:14 +0900

    lv1 이상한 문자 만들기 완

--- src/com/algorithm/programmers/lv1/이상한_문자_만들기.java
+++ src/com/algorithm/programmers/lv1/이상한_문자_만들기.java
@@ -9,17 +9,17 @@ public class 이상한_문자_만들기 {
     }
 
     public static String solution() {
-        String s = "trydppgpgaf helloasdfga worldasdgasg";
+        String s = "try           hello world";
         String answer = "";
 
-        String[] word = s.split(" ");
-
+        String[] word = s.split(" ", -1);
+        System.out.println(Arrays.toString(word));
         for(int i = 0; i < word.length; i++) {
             for(int j = 0; j < word[i].length(); j++) {
-                if(j % 2 == 0)
-                    answer += word[i].substring(j, j+1).toUpperCase(Locale.ROOT);
-                else if(j % 2 == 1)
-                    answer += word[i].substring(j, j+1).toLowerCase(Locale.ROOT);
+                    if(j % 2 == 0)
+                        answer += word[i].substring(j, j+1).toUpperCase(Locale.ROOT);
+                    else if(j % 2 == 1)
+                        answer += word[i].substring(j, j+1).toLowerCase(Locale.ROOT);
             }
             if(i != word.length-1)
                 answer += " ";
