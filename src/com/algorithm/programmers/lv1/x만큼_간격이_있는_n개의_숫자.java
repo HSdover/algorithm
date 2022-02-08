f7bb57e9ff98d7c24d7f6ab1a228a483740c0800
user <dover0601@naver.com>
2022-02-09 08:54:46 +0900

    lv1 x만큼 간격이 있는 n개의 숫자 완

--- src/com/algorithm/programmers/lv1/x만큼_간격이_있는_n개의_숫자.java
+++ src/com/algorithm/programmers/lv1/x만큼_간격이_있는_n개의_숫자.java
@@ -1,4 +1,21 @@
 package com.algorithm.programmers.lv1;
 
+import java.util.Arrays;
+
 public class x만큼_간격이_있는_n개의_숫자 {
+    public static void main(String[] args) {
+        solution(2, 5);
+    }
+
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+
+        long sum = x;
+        for(int i = 0; i < n; i++) {
+            answer[i] = sum;
+            sum += x;
+        }
+
+        return answer;
+    }
 }
---
+++ src/com/algorithm/programmers/lv1/직사각형_별찍기.java
@@ -0,0 +1,4 @@
+package com.algorithm.programmers.lv1;
+
+public class 직사각형_별찍기 {
+}
