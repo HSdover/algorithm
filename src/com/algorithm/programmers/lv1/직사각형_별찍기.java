265bf88494e64eddb3d67157ee68710b05e9e564
user <dover0601@naver.com>
2022-02-09 09:09:04 +0900

    lv1 완

--- src/com/algorithm/programmers/lv1/직사각형_별찍기.java
+++ src/com/algorithm/programmers/lv1/직사각형_별찍기.java
@@ -1,4 +1,26 @@
 package com.algorithm.programmers.lv1;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class 직사각형_별찍기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        StringBuilder[] sba = new StringBuilder[b];
+        for(int i = 0; i < sba.length; i++)
+            sba[i] = new StringBuilder("");
+
+        for(int i = 0; i < b; i++) {
+            for(int j = 0; j < a; j++)
+                sba[i].append("*");
+        }
+
+        for(int i = 0; i < sba.length; i++)
+            System.out.println(sba[i]);
+    }
+
+
 }
