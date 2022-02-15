a412c63c5352aa95609713389f78ad135bf16ce8
user <dover0601@naver.com>
2022-02-15 09:07:33 +0900

    lv2 기능개발 완

--- src/com/algorithm/programmers/lv2/기능개발.java
+++ src/com/algorithm/programmers/lv2/기능개발.java
@@ -1,26 +1,42 @@
 package com.algorithm.programmers.lv2;
 
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class 기능개발 {
     public static void main(String[] args) {
         solution();
     }
 
-    public static int[] solution() {
-        int[] progresses = {93, 30, 55};
-        int[] speeds = {1, 30, 5};
-        int[] answer = {};
+    public static List<Integer> solution() {
+        int[] progresses = {95, 90, 99, 99, 80, 99};
+        int[] speeds = {1, 1, 1, 1, 1, 1};
+        List<Integer> answer = new ArrayList<>();
 
-        Stack<Integer> st = new Stack<>();
-        for(int i = progresses.length-1; i >= 0; i--)
-            st.push(progresses[i]);
+        List<Integer> progresses2 = new ArrayList<>();
+        List<Integer> speeds2 = new ArrayList<>();
+        for(int i = 0; i < progresses.length; i++) {
+            progresses2.add(progresses[i]);
+            speeds2.add(speeds[i]);
+        }
 
-        st.add(0, 20);
-        System.out.println(st);
+        int cnt = 0;
+        while(progresses2.size() != 0) {
+            for(int i = 0; i < progresses2.size(); i++)
+                progresses2.set(i, progresses2.get(i) + speeds2.get(i));
 
+            System.out.println(progresses2.get(0));
+            while(progresses2.size() != 0 && progresses2.get(0) >= 100) {
+                cnt++;
+                progresses2.remove(0);
+                speeds2.remove(0);
+            }
+
+            if(cnt != 0) {
+                answer.add(cnt);
+                cnt = 0;
+            }
+        }
+        System.out.println(answer);
         return answer;
     }
 }
