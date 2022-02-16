941725a97f07b9d0942a19cf285974ab12b846fd
user <dover0601@naver.com>
2022-02-16 18:01:05 +0900

    lv2 행렬 테두리 회전하기 완

--- src/com/algorithm/programmers/lv2/행렬_테두리_회전하기.java
+++ src/com/algorithm/programmers/lv2/행렬_테두리_회전하기.java
@@ -12,7 +12,7 @@ public class 행렬_테두리_회전하기 {
         int rows = 6;
         int columns = 6;
         int[][] queries = {{2,2,5,4},{3,3,6,6},{5,1,6,3}};
-        int[] answer = {};
+        int[] answer = new int[queries.length];
 
         int val = 1;
         int[][] sq = new int[rows][columns];
@@ -21,7 +21,25 @@ public class 행렬_테두리_회전하기 {
                 sq[i][j] = val++;
         }
 
+        for(int i = 0; i < queries.length; i++) {
+            int x1 = queries[i][0] - 1;
+            int y1 = queries[i][1] - 1;
+            int x2 = queries[i][2];
+            int y2 = queries[i][3];
+            // 테두리 값 구하기
+            for(int j = x1; j < x2; j++) {
+                for(int k = y1; k < y2; k++) {
+                    if(j == x1 || j == x2 - 1)
+                        row.add(sq[j][k]);
+                    else if(k == y1 || k == y2 - 1)
+                        column.add(sq[j][k]);
+                }
+            }
 
+
+
+
+        }
 
         return answer;
     }
