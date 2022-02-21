b030f2aff64832d0084f188d6ef5770a418088dd
user <dover0601@naver.com>
2022-02-21 11:07:23 +0900

    lv2 괄호 변환 완

--- src/com/algorithm/programmers/lv2/괄호_변환.java
+++ src/com/algorithm/programmers/lv2/괄호_변환.java
@@ -5,30 +5,62 @@ public class 괄호_변환 {
         solution("()))((()");
     }
 
+    // 그냥 문제에 주어진 조건을 잘 읽고 옮기면 되는데 혼자 고민에 빠져서 이틀이나 풀었음.. 쉬운 문제인데.. 문제를 잘 읽자
+    public static String recusive(StringBuilder w) {
+        StringBuilder u = new StringBuilder();
+        StringBuilder v = new StringBuilder();
+        // 올바른 문자열 판별 변수
+        int right = 0, left = 0;
+        boolean ok = true;
+        for(int i = 0; i < w.length(); i++) {
+            if(w.charAt(i) == ')') {
+                right++;
+                u.append(w.charAt(i));
+            }
+            else {
+                left++;
+                u.append((w.charAt(i)));
+            }
+
+            if(right > left)
+                ok = false;
+
+            if(right == left) {
+                v.append(w.substring(i+1));
+                // u가 올바른 괄호 문자열인 경우
+                if(ok) {
+                    u.append(recusive(v));
+                    break;
+                }
+                // u가 올바른 괄호 문자열이 아닌 경우
+                else {
+                    StringBuilder sb = new StringBuilder("(");
+                    sb.append(recusive(v));
+                    sb.append(")");
+                    u.deleteCharAt(0);
+                    u.deleteCharAt(u.length()-1);
+                    for(int j = 0; j < u.length(); j++) {
+                        if(u.charAt(j) == ')')
+                            u.setCharAt(j, '(');
+                        else
+                            u.setCharAt(j, ')');
+                    }
+                    sb.append(u);
+                    u = new StringBuilder(sb);
+                    break;
+                }
+            }
+        }
+        return u.toString();
+    }
     public static String solution(String p) {
         String answer = "";
 
-        char[] cp = p.toCharArray();
-        int left = 0;
-        int right = 0;
-        String regex = "";
-        for(int i = 0; i < cp.length; i++) {
-            if(cp[i] == '(') {
-                left++;
-                regex += cp[i];
-            }
-            else {
-                right++;
-                regex += cp[i];
-            }
-            if(left == right) {
-                StringBuilder r1 = new StringBuilder(regex.substring(0, regex.length()/2));
-                StringBuilder r2 = new StringBuilder(regex.substring(regex.length()/2));
-                System.out.println(r1);
-                System.out.println(r2.reverse());
+        // 문자열 정의
+        StringBuilder w = new StringBuilder(p);
+        answer = recusive(w);
+        System.out.println(answer);
 
-            }
-        }
         return answer;
     }
 }
