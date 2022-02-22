c8d1f3ce0303a7eb890532f5c6d4f80f35828349
user <dover0601@naver.com>
2022-02-22 17:59:54 +0900

    lv2 빛의 경로 사이클 ~ing

---
+++ src/com/algorithm/programmers/lv2/빛의_경로_사이클.java
@@ -0,0 +1,13 @@
+package com.algorithm.programmers.lv2;
+
+public class 빛의_경로_사이클 {
+    public static void main(String[] args) {
+        solution();
+    }
+
+    public static int[] solution() {
+        String[] grid;
+        int[] answer = {};
+        return answer;
+    }
+}
