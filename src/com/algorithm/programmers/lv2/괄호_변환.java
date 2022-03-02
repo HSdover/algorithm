b842a9084d5288ed4badd532ac3e46f67e27b615
HSdover <dover0601@naver.com>
2022-03-02 23:21:31 +0900

    lv1 전화번호 목록 완

--- src/com/algorithm/programmers/lv2/괄호_변환.java
+++ src/com/algorithm/programmers/lv2/괄호_변환.java
@@ -5,31 +5,60 @@ public class 괄호_변환 {
         solution("()))((()");
     }
 
-    private static String[] makeString(String p) {
-        String[] uv = new String[2];
-
+    // 그냥 문제에 주어진 조건을 잘 읽고 옮기면 되는데 혼자 고민에 빠져서 이틀이나 풀었음.. 쉬운 문제인데.. 문제를 잘 읽자
+    public static String recusive(StringBuilder w) {
+        StringBuilder u = new StringBuilder();
+        StringBuilder v = new StringBuilder();
+        // 올바른 문자열 판별 변수
         int right = 0, left = 0;
-        StringBuilder sb = new StringBuilder(p);
-        for(int i = 0; i < p.length(); i++) {
-            if(sb.charAt(i) == ')') {
+        boolean ok = true;
+        for(int i = 0; i < w.length(); i++) {
+            if (w.charAt(i) == ')') {
                 right++;
-                uv[0] += sb.charAt(i);
-            }
-            else {
+                u.append(w.charAt(i));
+            } else {
                 left++;
-                uv[0] += sb.charAt(i);
+                u.append((w.charAt(i)));
+            }
+
+            if(right > left)
+                ok = false;
+
+            if(right == left) {
+                v.append(w.substring(i+1));
+                // u가 올바른 괄호 문자열인 경우
+                if(ok) {
+                    u.append(recusive(v));
+                    break;
+                }
+                // u가 올바른 괄호 문자열이 아닌 경우
+                else {
+                    StringBuilder sb = new StringBuilder("(");
+                    sb.append(recusive(v));
+                    sb.append(")");
+                    u.deleteCharAt(0);
+                    u.deleteCharAt(u.length()-1);
+                    for(int j = 0; j < u.length(); j++) {
+                        if(u.charAt(j) == ')')
+                            u.setCharAt(j, '(');
+                        else
+                            u.setCharAt(j, ')');
+                    }
+                    sb.append(u);
+                    u = new StringBuilder(sb);
+                    break;
+                }
             }
         }
-
-        return uv;
+        return u.toString();
     }
-
     public static String solution(String p) {
         String answer = "";
 
-        String u = new String();
-        String v = new String();
-
+        // 문자열 정의
+        StringBuilder w = new StringBuilder(p);
+        answer = recusive(w);
+        System.out.println(answer);
 
         return answer;
     }
--- src/com/algorithm/programmers/lv2/전화번호_목록.java
+++ src/com/algorithm/programmers/lv2/전화번호_목록.java
@@ -14,9 +14,10 @@ public class 전화번호_목록 {
         String[] phone_book = {"123", "125", "123523", "123982"};
         boolean answer = true;
 
-        Arrays.sort(phone_book, Com);
-        for(int i = 0; i < phone_book.length; i++) {
-            for(int j = 0; j < phone_book.length; j++) {
+        // String정렬이므로 바로 다음 녀석이 안맞으면 바로 넘겨도 된다..
+        Arrays.sort(phone_book);
+        for(int i = 0; i < phone_book.length-1; i++) {
+            for(int j = i; j < i+2; j++) {
                 if(i != j) {
                     if(phone_book[j].startsWith(phone_book[i])) {
                         answer = false;
