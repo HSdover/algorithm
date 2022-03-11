80066045bd34df16fa3f4e13106979df5661aa87
HAN <dover0601@naver.com>
2022-03-12 07:37:48 +0900

    lv2 가장 큰 수 완

---
+++ src/com/algorithm/programmers/lv2/소수_찾기.java
@@ -0,0 +1,12 @@
+package com.algorithm.programmers.lv2;
+
+public class 소수_찾기 {
+    public static void main(String[] args) {
+        solution("011");
+    }
+
+    public static int solution(String numbers) {
+        int answer = 0;
+        return answer;
+    }
+}
