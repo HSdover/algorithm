7c09b1e85819048658240ff8286320357083a30c
user <dover0601@naver.com>
2022-03-16 09:47:12 +0900

    lv2 다리를 지나는 트럭 완

--- src/com/algorithm/programmers/lv2/다리를_지나는_트럭.java
+++ src/com/algorithm/programmers/lv2/다리를_지나는_트럭.java
@@ -7,33 +7,35 @@ import java.util.Stack;
 public class 다리를_지나는_트럭 {
     public static void main(String[] args) {
         다리를_지나는_트럭 t = new 다리를_지나는_트럭();
-        int[] truck_weights = {10};
-        t.solution(100, 100, truck_weights);
+        int[] truck_weights = {7,4,5,6};
+        t.solution(2, 10, truck_weights);
     }
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int answer = 0;
 
         Queue<Integer> q = new LinkedList<>();
+        q.offer(0);
         int w = 0, i = 0;
 
-        while(i != truck_weights.length) {
+        while(q.size() != 0) {
+            if(answer == 0)
+                q.poll();
+
             answer++;
 
-            if(q.size() > bridge_length)
+            if(answer > answer % (bridge_length+1))
                 w -= q.poll();
 
-            if(w + truck_weights[i] <= weight) {
+            if(i < truck_weights.length && w + truck_weights[i] <= weight) {
                 q.offer(truck_weights[i]);
-                w += truck_weights[i];
-                i++;
+                w += truck_weights[i++];
             }
-            else
+            else if(i != truck_weights.length)
                 q.offer(0);
+            System.out.println(q);
         }
-        System.out.println(q.size());
-        System.out.println(q.peek());
-        answer += bridge_length;
+
         System.out.println(answer);
         return answer;
     }
